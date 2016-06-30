ba9e971b521dd23cdc0abfd26cea6e40441988e0
Walker <jiabinxu_dx@163.com>
2016-06-30 17:06:57 +0800

    对应将这件物品装包时的价值

--- 01背包问题.cpp
+++ 01背包问题.cpp
@@ -14,7 +14,7 @@ int main(){
 	for(int i=0;i<=max_num;i++){
 		for(int j=0;j<=max_weight;j++){
 			if(i==0||j==0) dp[i][j]=0;			
-			else dp[i][j]=max(dp[i-1][j-weight[i]]+weight[i],dp[i-1][j]);
+			else dp[i][j]=max(dp[i-1][j-weight[i]]+val[i],dp[i-1][j]);
 
 	}
 	
