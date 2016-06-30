93953c4a70032646c44247c8790b38ecff897890
Walker <jiabinxu_dx@163.com>
2016-06-30 17:12:05 +0800

    01背包

--- 01背包问题.cpp
+++ 01背包问题.cpp
@@ -13,8 +13,8 @@ int main(){
 
 	for(int i=0;i<=max_num;i++){
 		for(int j=0;j<=max_weight;j++){
-			if(i==0||j==0) dp[i][j]=0;			
-			else dp[i][j]=max(dp[i-1][j-weight[i]]+val[i],dp[i-1][j]);
+			if(j<weight[i]) dp[i][j]=dp[i-1][j];//这件货物不装包
+			else dp[i][j]=max(dp[i-1][j-weight[i]]+val[i],dp[i-1][j]);//装包价值和不装包价值对比
 
 	}
 	
